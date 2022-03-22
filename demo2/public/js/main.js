e990b9d7a13ba72dc5f3b2e58bc18bd26275190e
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-03-22 11:50:52 +0100

    del folders demo

--- demo2/public/js/main.js
+++
@@ -1,3 +0,0 @@
-let para = document.getElementsByClassName('intro')[0]; // le 0 cible le premier element du tableau car il existe plusieurs paragraphe avec la classe intro.
-para.innerHTML = "coucou petite perruche";
-console.log(para);
